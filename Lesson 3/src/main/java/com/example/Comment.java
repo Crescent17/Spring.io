99f2508f81106853d23e41944499570e58aefdae
Marat <94042173+Crescent17@users.noreply.github.com>
2022-01-17 16:21:56 +0300

    Add files via upload

---
+++ Lesson 3/src/main/java/com/example/Comment.java
@@ -0,0 +1,66 @@
+package com.example;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Comment {
+    private int postId;
+    private int id;
+    private String name;
+    private String email;
+    private String body;
+
+    public Comment() {
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "postId=" + postId +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
